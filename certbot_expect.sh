20cbe16f2039436a8fc932637816e845a49e130d
zhaozuodong <tenjor@163.com>
2024-04-24 19:25:27 +0800

    fix

--- certbot_expect.sh
+++ certbot_expect.sh
@@ -6,10 +6,12 @@ spawn certbot --nginx --register-unsafely-without-email
 
 expect {
     "Cert not yet due for renewal" {
-        exit 0
+        send "c"
+        exp_continue
     }
     "No names were found in your configuration files" {
-        exit 0
+        send "c"
+        exp_continue
     }
     "Select the appropriate number" {
         send "\n"
